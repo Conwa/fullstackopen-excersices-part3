9697580d18c4ff3bbf94b9192046bd55758d87c3
Conwa <conradodelcarlo97@gmail.com>
2022-11-07 20:43:01 -0300

    3.2 to 3.6 completed :white_check_mark:

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 
-const persons = [
+let phonebook = [
   {
     id: 1,
     name: "Arto Hellas",
@@ -28,67 +28,78 @@ const persons = [
 
 /*METODOS*/
 
+/*first method*/
 app.get("/", (request, response) => {
   response.send("<h1>Hello World!</h1>");
 });
 
+/*get all persons*/
 app.get("/api/persons", (request, response) => {
-  response.json(persons);
+  response.json(phonebook);
 });
 
-app.get("/api/info", (request, response) => {
-  const persons = 100;
+/*get some info*/
+app.get("/info", (request, response) => {
   const date = new Date();
+  const personsCount = Object.keys(phonebook).length;
   response.send(
     `  <div>
-      <h2>Phonebook has info for ${persons} people</h2>
+    <h2>Phonebook has info for ${personsCount} people</h2>
       <h2>${date}</h2>
     </div>`
   );
-  response.send(`<h2>Phonebook has info for ${persons} people</h2>`);
 });
 
-app.get("/api/notes/:id", (request, response) => {
+/*get specific person*/
+app.get("/api/persons/:id", (request, response) => {
   const id = Number(request.params.id);
-  const note = notes.find((note) => note.id === id);
-  if (note) {
-    response.json(note);
+  const person = phonebook.find((person) => person.id === id);
+  if (person) {
+    response.json(person);
   } else {
     response.status(404).end();
   }
 });
 
-const generateId = () => {
-  const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
-  return maxId + 1;
-};
-
-app.post("/api/notes", (request, response) => {
-  const body = request.body;
-
-  if (!body.content) {
-    return response.status(400).json({
-      error: "content missing",
-    });
-  }
-
-  const note = {
-    id: generateId(),
-    content: body.content,
-    date: new Date(),
-    important: body.important || false,
-  };
-
-  notes = notes.concat(note);
-
-  response.json(note);
-});
-
-app.delete("/api/notes/:id", (request, response) => {
+/*delete person by ID*/
+app.delete("/api/persons/:id", (request, response) => {
   const id = Number(request.params.id);
-  notes = notes.filter((note) => note.id !== id);
+  phonebook = phonebook.filter((person) => person.id !== id);
 
   response.status(204).end();
+});
+
+/*post new person*/
+const generateId = () => {
+  const newId = Math.floor(Math.random() * 100000 + 1);
+  return newId;
+};
+app.post("/api/persons", (request, response) => {
+  const body = request.body;
+  console.log(body);
+
+  if (!body.name) {
+    return response.status(400).json({ error: "name missing" });
+  }
+  if (!body.number) {
+    return response.status(400).json({ error: "number missing" });
+  }
+  const aMatch = phonebook.some((person) => {
+    return person.name.toLowerCase() === body.name.toLowerCase();
+  });
+  if (aMatch) {
+    return response.status(400).json({ error: "name already added" });
+  }
+
+  const person = {
+    id: generateId(),
+    name: body.name,
+    number: body.number || "unknown",
+  };
+
+  phonebook = phonebook.concat(person);
+
+  response.json(phonebook);
 });
 
 const PORT = 3001;
